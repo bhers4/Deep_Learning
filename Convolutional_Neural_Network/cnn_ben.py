a16011a3813867268ccd457aa2639e5644411e07
Ben Hers <bhers4@gmail.com>
2018-09-16 21:35:34 -0700

    finished CNN and added second Convolutional layer and test prediction

--- Convolutional_Neural_Network/cnn_ben.py
+++ Convolutional_Neural_Network/cnn_ben.py
@@ -6,6 +6,7 @@ from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 from keras.preprocessing import image
+import PIL
 
 # Initialize Convolutional Neural Network
 classifier = Sequential()
@@ -16,6 +17,11 @@ classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size=(2,2)))
+
+### Add second convolutional layer to increase accuracy
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size=(2,2)))
+
 
 # Step 3 - Flattening
 classifier.add(Flatten())
@@ -31,3 +37,22 @@ classifier.add(Dense(units = 1, activation = 'sigmoid'))
 # Binary Cross entropy is binary logarthimc loss function, so we minimize this loss function
 classifier.compile(optimizer='adam', loss='binary_crossentropy' , metrics=['accuracy'])
 
+### Code from Keras Documentation
+train_datagen = ImageDataGenerator(rescale = 1./255,shear_range = 0.2,zoom_range = 0.2,horizontal_flip = True)
+test_datagen = ImageDataGenerator(rescale = 1./255)
+training_set = train_datagen.flow_from_directory('dataset/training_set',target_size = (64, 64),batch_size = 32,class_mode = 'binary')
+test_set = test_datagen.flow_from_directory('dataset/test_set',target_size = (64, 64),batch_size = 32,class_mode = 'binary')
+classifier.fit_generator(training_set,steps_per_epoch = 8000,epochs = 25,validation_data = test_set,validation_steps = 2000)
+# End of Keras Code
+
+print("End of CNN")
+
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
